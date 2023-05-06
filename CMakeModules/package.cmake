55a0e0906e7e2aa34143aa8d44cffdf3e9853751
LuckyDev <heliosyamet3190@outlook.com>
2023-05-06 04:15:57 +0200

    proton
    proton

---
+++ CMakeModules/package.cmake
@@ -0,0 +1,11 @@
+set(VENDOR "protonchain.com")
+set(PROJECT_NAME "proton")
+set(DESC "Software for the Proton network")
+set(URL "https://github.com/ProtonProtocol/proton")
+set(EMAIL "hello@protonchain.com")
+
+configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_package.sh.in ${CMAKE_BINARY_DIR}/packages/generate_package.sh @ONLY)
+configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_bottle.sh ${CMAKE_BINARY_DIR}/packages/generate_bottle.sh COPYONLY)
+configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_deb.sh ${CMAKE_BINARY_DIR}/packages/generate_deb.sh COPYONLY)
+configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_rpm.sh ${CMAKE_BINARY_DIR}/packages/generate_rpm.sh COPYONLY)
+configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_tarball.sh ${CMAKE_BINARY_DIR}/packages/generate_tarball.sh COPYONLY)
